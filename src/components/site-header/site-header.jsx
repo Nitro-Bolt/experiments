784dd3d7a9caee97892511f6aed913b2ebaf9e43
Cubester <cubester.spamlegends@gmail.com>
2026-03-17 00:23:00 -0400

    Global Procedures Experiment

--- src/components/site-header/site-header.jsx
+++ src/components/site-header/site-header.jsx
@@ -3,7 +3,7 @@ import "./site-header.css";
 const SiteHeader = () => (
   <header className="site-header">
     <h1>NitroBolt Experiments</h1>
-    <p>Testing zone the for big, upcoming features that we're working on!</p>
+    <p>Testing zone for the big, upcoming features that we're working on!</p>
   </header>
 );
 
